901f86d4bfee3aecb578c412d1ae704fd2d0bd52
lekhanh-dev <levankhanh.dev@gmail.com>
2020-02-24 22:08:00 +0700

    Updates

---
+++ precache-manifest.bc1d83346f9e3d279747e4ba7851d29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63dde892eb99ea246ca48d368d98c56",
+    "url": "/tic-tac-toe-react-app/index.html"
+  },
+  {
+    "revision": "b0d4f9b04f2b8c333533",
+    "url": "/tic-tac-toe-react-app/static/css/main.63bcfe58.chunk.css"
+  },
+  {
+    "revision": "0be154a3e0c083d1f41f",
+    "url": "/tic-tac-toe-react-app/static/js/2.718ed629.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe-react-app/static/js/2.718ed629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d4f9b04f2b8c333533",
+    "url": "/tic-tac-toe-react-app/static/js/main.03ce5e93.chunk.js"
+  },
+  {
+    "revision": "b5791ed959cfb4981eea",
+    "url": "/tic-tac-toe-react-app/static/js/runtime-main.438f7f90.js"
+  }
+]);
